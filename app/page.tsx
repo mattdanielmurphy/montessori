22d49b8b7fb4c425384e29f57c1b8676a16f2655
Matthew Daniel Murphy <matt@mattmurphy.ca>
2023-12-17 08:09:24 +0000

    Improve and add more playlists

--- app/page.tsx
+++ app/page.tsx
@@ -1,76 +1,150 @@
 "use client"
 import Image from "next/image"
 import { useEffect, useState } from "react"
-
-interface Playlist {
-	id: string
-	name: string
-}
-
-const playlists: Playlist[] = [
-	{ id: "PLGXNehBPAYVYIpRQhT-On1gfD3SzarGLr", name: "Intro" },
-	{
-		id: "PLGXNehBPAYVYPLT0o9FCs7ruCuFjGBz8y",
-		name: "The Prepared Environment",
-	},
-	{
-		id: "PLGXNehBPAYVYvJjtkyTjcAsvk5_Fb2ToA",
-		name: "Laura",
-	},
-]
+import YouTube, { YouTubeProps } from "react-youtube"
+import { Handout, playlists } from "./playlists"
 
 export default function Home() {
 	const [moduleIndex, setModuleIndex] = useState(0)
+	const [videoIndex, setVideoIndex] = useState(0)
+	const [isLoading, setIsLoading] = useState(true)
+	const [indiciesPlayedTo, setIndiciesPlayedTo] = useState<{
+		[index: number]: number
+	}>({ 0: 1 })
 	const [moduleName, setModuleName] = useState(playlists[moduleIndex].name)
+	const accentColor = "red" // Youtube red (pretty sick that it's just pure red)
 
 	useEffect(() => {
+		setIsLoading(true)
 		setModuleName(playlists[moduleIndex].name)
 	}, [moduleIndex])
 
-	const lastIndex = playlists.length-1
+	// const lastIndex = playlists.length - 1
+	const lastIndex = 7
 
-	function handleNextButton() {
-		if (moduleIndex + 1 <= lastIndex) setModuleIndex(moduleIndex + 1)
-	}
 	function handlePrevButton() {
 		if (moduleIndex - 1 >= 0) setModuleIndex(moduleIndex - 1)
 	}
+	const onReady: YouTubeProps["onReady"] = (event) => {
+		// access to player in all event handlers via event.target
+		console.log("ready!")
+		console.log("cueing with index:", indiciesPlayedTo[moduleIndex])
 
+		const newVideoIndex = indiciesPlayedTo[moduleIndex] || 0
+		event.target.cuePlaylist(playlists[moduleIndex].id, newVideoIndex, 0)
+		setVideoIndex(newVideoIndex)
+		setIsLoading(false)
+	}
+	const onEnd: YouTubeProps["onEnd"] = (event) => {
+		// fires when playlist ends
+		console.log("ENDED, marking as played")
+		const newIndicies = indiciesPlayedTo
+		newIndicies[moduleIndex] = videoIndex
+		setIndiciesPlayedTo(newIndicies)
+		// go to next module
+		setModuleIndex(moduleIndex + 1)
+	}
+	const onPlay: YouTubeProps["onPlay"] = (event) => {
+		// access to player in all event handlers via event.target
+		console.log("event.target", event.target)
+		if (videoIndex) {
+			const newIndicies = indiciesPlayedTo
+			newIndicies[moduleIndex] = videoIndex
+			setIndiciesPlayedTo(newIndicies)
+		}
+		setVideoIndex(event.target.getPlaylistIndex())
+		console.log("next video ready")
+		setIsLoading(false)
+	}
 	return (
-		<main className='flex min-h-screen flex-col items-center justify-between py-12 px-24'>
-			<h1>Montessori Beginnings</h1>
-			{/* <iframe
-        src={`https://mega.nz/embed/${videos[moduleIndex].id}`}
-        allowFullScreen
-      ></iframe> */}
+		<main className='flex min-h-screen flex-col items-center justify-center py-12 px-24'>
+			{isLoading && (
+				<div className='spinner'>
+					<div />
+				</div>
+			)}
+			<style jsx>{`
+				.spinner {
+					--height: 40px;
+					margin-top: calc((360 / 2) + 40);
+					width: 40px;
+					height: var(--height);
+					position: absolute;
+					border: 1.5px solid rgba(0, 0, 0, 0.2);
+					border-radius: 50%;
+					border-top: 4px solid ${accentColor};
+					animation: spin 0.5s linear infinite;
+					margin-bottom: 10px;
+					z-index: 999;
+				}
 
-			<iframe
-				src={`https://www.youtube.com/embed/videoseries?list=${playlists[moduleIndex].id}`}
-				title='YouTube video player'
-				allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-				allowFullScreen
-			></iframe>
+				@keyframes spin {
+					0% {
+						transform: rotate(0deg);
+					}
+					100% {
+						transform: rotate(360deg);
+					}
+				}
+			`}</style>
+			<h1>Montessori Beginnings</h1>
+			<YouTube
+				onEnd={onEnd} // defaults -> noop
+				onPlay={onPlay} // defaults -> noop
+				onReady={onReady}
+				opts={{
+					playerVars: {
+						listType: "playlist",
+						list: playlists[moduleIndex].id,
+					},
+				}}
+			/>
 			<div className='mt-10 text-center flex items-center space-x-10'>
-				<button onClick={handlePrevButton} className='group'>
-					<h3 className={`text-3xl`}>
-						<span className='inline-block transition-transform group-hover:scale-150 motion-reduce:transform-none'>
-							&lt;-
-						</span>
-					</h3>
-				</button>
+					<button disabled={moduleIndex - 1 < 0} className='prev-button' onClick={() => setModuleIndex(moduleIndex - 1)} >
+						<h3 className={`text-3xl`}>
+							<span className='inline-block transition-transform group-hover:scale-150 motion-reduce:transform-none'>
+								&lt;-
+							</span>
+						</h3>
+					</button>
 				<h2>
 					Module {moduleIndex + 1}
 					{moduleName && `: ${moduleName}`}
 				</h2>
-				<button onClick={handleNextButton} className='group'>
-					<h3 className={`text-3xl`}>
-						<span className='inline-block transition-transform group-hover:scale-150 motion-reduce:transform-none'>
-							-&gt;
-						</span>
-					</h3>
-				</button>
+					<button disabled={moduleIndex + 1 > lastIndex} className='next-button'
+						onClick={() => setModuleIndex(moduleIndex + 1)}
+					>
+						<h3 className={`text-3xl`}>
+							<span className='inline-block transition-transform group-hover:scale-150 motion-reduce:transform-none'>
+								-&gt;
+							</span>
+						</h3>
+					</button>
+
 			</div>
+			{playlists[moduleIndex].workbookURL && (
+				<p>
+					<a target='_blank' href={playlists[moduleIndex].workbookURL}>
+						↓ Workbook
+					</a>
+				</p>
+			)}
+			{playlists[moduleIndex].handouts && (
+				<>
+					<p>Handouts:</p>
+					<p>
+						{playlists[moduleIndex].handouts?.map((handout: Handout) => (
+							<a target='_blank' href={handout.url}>
+								↓ "{handout.name}"
+							</a>
+						))}
+					</p>
+				</>
+			)}
 			<style jsx>{`
+				a {
+					color: hsl(200, 100%, 50%);
+				}
 				h1 {
 					font-family: "Avenir", "Helvetica Neue", Helvetica, Arial, sans-serif;
 					font-size: 2.4rem;
@@ -78,17 +152,15 @@ export default function Home() {
 					margin-bottom: 2rem;
 					border-radius: 0.5rem;
 				}
-				iframe {
-					width: 100%;
-					aspect-ratio: 16 / 9;
-					max-width: 1280px;
-				}
 				h2 {
 					font-family: "Avenir", "Helvetica Neue", Helvetica, Arial, sans-serif;
 					letter-spacing: 0.1rem;
 					text-transform: uppercase;
 					font-size: 1.8rem;
 					margin-top: 0.4rem;
+				}
+				button:disabled {
+					color: black;
 				}
 			`}</style>
 		</main>
---
+++ app/playlists.tsx
@@ -0,0 +1,91 @@
+"use client"
+export interface Handout { url: string; name: string }
+
+interface Playlist {
+	id: string
+	name: string
+	workbookURL: string
+	handouts?: Handout[]
+}
+
+export const playlists: Playlist[] = [
+	{
+		id: "PLGXNehBPAYVYIpRQhT-On1gfD3SzarGLr",
+		name: "Intro",
+		workbookURL:
+			"https://mega.nz/file/lpUEzRha#twnFFTCAwr9G5CypycSJ06yFJXMSGVEkAqugK6ORkWI",
+	},
+	{
+		id: "PLGXNehBPAYVYPLT0o9FCs7ruCuFjGBz8y",
+		name: "The Prepared Environment",
+		workbookURL:
+			"https://mega.nz/file/g1UBiJbI#mgKsaTBI4GZGWL1A8yxwv1PNjJBY1Zg_naakjQIYxMM",
+	},
+	{
+		id: "PLGXNehBPAYVYvJjtkyTjcAsvk5_Fb2ToA",
+		name: "Preparing Ourselves as Adults",
+		workbookURL:
+			"https://mega.nz/file/F512kZZT#tqlwzZXYpBcNJwBaqUOAlZjg9hAVAZXp6MNupOWsdfE",
+	},
+	{
+		id: "PLGXNehBPAYVY5dILxTbsXxxaXCz_e5d6X",
+		name: "Observation",
+		workbookURL:
+			"https://mega.nz/file/w01REKrB#PYpNqWDJpzdV-NUw7DB1H1GjZvOu3RgUenk26jCOERg",
+	},
+	{
+		id: "PLGXNehBPAYVY0CZx7X8rC2vQSZHLoR3vy",
+		name: "Social-Emotional Development",
+		workbookURL:
+			"https://mega.nz/file/d01mEYzZ#7Q3Q4sBJD_Q21gVd2WrUi1wTUOPeIDF_KsvPlbsCRhw",
+	},
+	{
+		id: "PLGXNehBPAYVZonih5k4MuMtOGWh8FN018",
+		name: "",
+		workbookURL:
+			"https://mega.nz/file/FsNWUbKI#eZhk4bo9BEyzq8m57E38za_mIHNGvuE4-PjoaE3Rr0Y",
+		handouts: [
+			{
+				name: "Child's BBB",
+				url: "https://mega.nz/file/w8MwmJ5b#Am_trZS_t5K-JyZ0SEwyfduznaW7oi6-LYJspXaCHKc",
+			},
+		],
+	}, // week 6
+	{
+		id: "PLGXNehBPAYVagjhehL5ATqYz0ulOzk528",
+		name: "",
+		workbookURL:
+			"https://mega.nz/file/N8NBxBwA#tRm9p2HGY2ScZnD-JK6Rs_0bFil8hWbvIJhZJVT5b8w",
+	}, // week 7
+	{
+		id: "PLGXNehBPAYVZgRgoLrTtKRFBHsAl1rIVl",
+		name: "",
+		workbookURL:
+			"https://mega.nz/file/RgElQDAa#mWOkBVKX4EOIOZC3rQFnLUJ78GYLUNyUR2uNCZMummE",
+	}, // week 8
+	{
+		id: "",
+		name: "Language Development",
+		workbookURL:
+			"https://mega.nz/file/Bp0jCZCA#r2UoS-pwn9e5rAbd3sc3d5Kdk7UHZPY8kazkmfgOqtY",
+	}, // week 9
+	{
+		id: "",
+		name: "Sensorial Activities",
+		workbookURL:
+			"https://mega.nz/file/o0kn0Q6A#qnPqSJP08DgfRoeVO6qLdYvN8RRJuLE2CaRNygo20wQ",
+	}, // week 10
+	{
+		id: "",
+		name: "Covering Basic Needs",
+		workbookURL:
+			"https://mega.nz/file/FsFT3IRB#E8P2CKi6bad78w9mZIEiB6TzKFHFZ-bYptvRMC1fGSc",
+	}, // week 11
+	{
+		id: "",
+		name: "Toilet Learning",
+		workbookURL:
+			"https://mega.nz/file/ttcGEKBb#2d1jEi7Gfi_7qkwlJfdC3toFFnoljPHDpJZzfWY23k0",
+	}, // week 12
+	{ id: "", name: "Bonus", workbookURL: "" },
+]
